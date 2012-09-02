9ab28b54f7301e763aa526c7bbe1a2ee180bf769
Matthew Closson <matthew.closson@gmail.com>
2012-09-02 19:19:01 -0400

    Add tests for invalid token or no token provided

--- test/functional/tickets_controller_test.rb
+++ test/functional/tickets_controller_test.rb
@@ -6,10 +6,10 @@ class TicketsControllerTest < ActionController::TestCase
     @controller = Api::V1::TicketsController.new
     @request.env['Accept'] = 'application/json'
     @request.env['Content-type'] = 'application/json'
-    @request.env['HTTP_AUTHORIZATION'] = 'Token token="ABCDEF0123456789"'
   end
 
   def test_successfully_create_ticket
+    @request.env['HTTP_AUTHORIZATION'] = 'Token token="ABCDEF0123456789"'    
     parameters = {location: 'testlocation'}
     post(:create, parameters)    
     assert_response :created
@@ -17,9 +17,23 @@ class TicketsControllerTest < ActionController::TestCase
   end
 
   def test_create_ticket_no_location_given
+    @request.env['HTTP_AUTHORIZATION'] = 'Token token="ABCDEF0123456789"'    
     post :create    
     assert_response :unprocessable_entity
     assert_equal '{"error":"Could not create ticket becase..."}', @response.body
   end
 
+  def test_invalid_token_unauthorized
+    @request.env['HTTP_AUTHORIZATION'] = 'Token token="BADTOKEN"'    
+    parameters = {location: 'testlocation'}
+    post(:create, parameters)
+    assert_response :unauthorized
+  end
+
+  def test_no_token_unauthorized
+    parameters = {location: 'testlocation'}
+    post(:create, parameters)
+    assert_response :unauthorized
+  end
+      
 end
