d530e5e3d40861ff051968a7e43abd4d484fcaaf
Matthew Closson <matthew.closson@gmail.com>
2012-09-05 10:52:09 -0400

    Add relationship tests for account and location

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @account = accounts(:ingen)
+  end
+
+  def test_has_many_locations
+    assert_respond_to(@account, :locations)
+    assert_instance_of(Array, @account.locations)
+  end
+
+  def test_has_many_users
+    assert_respond_to(@account, :users)
+    assert_instance_of(Array, @account.users)
+  end
+
 end
--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @location = locations(:sorna)
+  end
+
+  def test_has_many_sales_tickets
+    assert_respond_to(@location, :sales_tickets)
+    assert_instance_of(Array, @location.sales_tickets)
+  end
+
+  def test_has_many_employee_clock_ins
+    assert_respond_to(@location, :employee_clock_ins)
+    assert_instance_of(Array, @location.employee_clock_ins)
+  end    
+
+  def test_belongs_to_account
+    assert_respond_to(@location, :account)
+    assert_instance_of(Account, @location.account)
+  end
+
 end
