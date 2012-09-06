60340039f76c9b57d01912316fda4b1c8f1a0b44
Matthew Closson <matthew.closson@gmail.com>
2012-09-06 04:25:46 -0300

    Added Integration checkout workflow test
    Added test to:
    login, clockin, create ticket, clock out, logout
    It even works!
    rake test
    or rake test:integration

--- test/integration/checkout_workflow_test.rb
+++ test/integration/checkout_workflow_test.rb
@@ -2,9 +2,43 @@ require 'test_helper'
  
 class CheckoutWorkflowTest < ActionDispatch::IntegrationTest
  
-  # test "login and create a ticket" do
-  #   post "/api/v1/sessions"
-  #   assert_response :success
-  # end
+  test "login, clockin and create a ticket, clockout, logout" do
+    request_headers = Hash.new
+    request_headers['Accept'] = 'application/json'
+    request_headers['Content-type'] = 'application/json'
+
+    request_parameters = Hash.new
+    request_parameters[:username] = 'mclosson'
+    request_parameters[:password] = 'mpass'
+
+    post("/api/v1/sessions", request_parameters, request_headers)
+    assert_response :created
+    assert_match /{"token":".*"}/, @response.body
+    json = ActiveSupport::JSON.decode(@response.body)
+    token = json['token']
+
+    request_headers['HTTP_AUTHORIZATION'] = 'Token token="' + token + '"'
+    request_parameters = Hash.new
+    post("/api/v1/sessions/clock", request_parameters, request_headers)
+    assert_response :ok
+    assert_match /{'time':'.*'}/, @response.body
+
+    post("/api/v1/tickets", request_parameters, request_headers)
+    assert_response :created
+    assert_match /{"ticket_id":".*"}/, @response.body
+
+    #TODO: Add line items to ticket
+    #TODO: Remove a line item from the ticket
+    #TODO: Collect payments
+    #TODO: Totalize items and issue a reciept
+
+    delete("/api/v1/sessions/clock", request_parameters, request_headers)
+    assert_response :ok
+    assert_match /{"time_worked":".*"}/, @response.body
+
+    delete("/api/v1/sessions", request_parameters, request_headers)
+    assert_response :ok
+    assert_match /{"message":"Logged out"}/, @response.body
+  end
 
 end
