8373a6f7af96fff41422d846f3777a329c90dc83
Matthew Closson <matthew.closson@gmail.com>
2012-09-12 11:55:37 -0400

    Removed duplicated token handling logic
    Refactored ApiController and SessionsController to move token
    authentication logic into a single location the new TokenAuthenticatable
    module.  Now in order to require all of a controller's methods use a
    token you have it inherit from ApiController.  However to just specify
    some of your methods require a token but not all, you can include
    TokenAuthenticatable.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   
+  include TokenAuthenticatable
   respond_to :json
+
+  # The method :restrict_access which is called in this before_filter is
+  # defined in the TokenAuthenticatable module we included above
   before_filter :restrict_access, only: [:destroy, :clock_in, :clock_out]
 
   def create
@@ -65,44 +69,6 @@ class Api::V1::SessionsController < ApplicationController
   def authenticate(username, password)
     user = User.find_by_username(username)
     user.nil? ? false : user.authenticate(password)
-  end
-
-  # For most other controllers this method is defined and inherited
-  # from api_controller, however sessions_controller does not inherit
-  # from api_controller so we will replicate this check here for specific
-  # methods like clock_in and clock_out
-
-  # Look up the token in the cache first, if not found look
-  # it up in the database.  If it is found cache the result
-  # and return true to allow the request.  If it is still not
-  # found in the database then we have someone playing games
-  # and we need to return false reject the request as invalid.
-  # It could also be someone interacting with the API wrong.
-  def restrict_access
-    authenticate_or_request_with_http_token do |token, options|
-      token_cached?(token) || find_and_cache_token(token)
-    end
-  end
-
-  def token_cached?(token)
-    hash = Rails::cache.read("apikey_#{token}")    
-    return false unless hash
-    @apikey = ApiKey.new(
-      id: hash[:id], access_token: hash[:access_token], 
-      location_id: hash[:location_id], user_id: hash[:user_id]
-    )
-    return @apikey.access_token == token
-  end
-
-  def find_and_cache_token(token)
-    begin
-      @apikey = ApiKey.find_by_access_token(token)
-      Rails::cache.write("apikey_#{token}", @apikey) unless @apikey.nil?
-    rescue ActiveRecord::RecordNotFound
-      @apikey = nil
-    ensure
-      return !@apikey.nil?
-    end
   end
 
 end
---
+++ app/models/token_authenticatable.rb
@@ -0,0 +1,38 @@
+module TokenAuthenticatable
+
+  private
+
+  # Look up the token in the cache first, if not found look
+  # it up in the database.  If it is found cache the result
+  # and return true to allow the request.  If it is still not
+  # found in the database then we have someone playing games
+  # and we need to return false reject the request as invalid.
+  # It could also be someone interacting with the API wrong.
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      token_cached?(token) || find_and_cache_token(token)
+    end
+  end
+
+  def token_cached?(token)
+    hash = Rails::cache.read("apikey_#{token}")    
+    return false unless hash
+    @apikey = ApiKey.new(
+      id: hash[:id], access_token: hash[:access_token], 
+      location_id: hash[:location_id], user_id: hash[:user_id]
+    )
+    return @apikey.access_token == token
+  end
+
+  def find_and_cache_token(token)
+    begin
+      @apikey = ApiKey.find_by_access_token(token)
+      Rails::cache.write("apikey_#{token}", @apikey) unless @apikey.nil?
+    rescue ActiveRecord::RecordNotFound
+      @apikey = nil
+    ensure
+      return !@apikey.nil?
+    end
+  end
+
+end
