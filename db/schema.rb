4482fde7b367312cf35ef3a6a33c8b61098367be
Matthew Closson <matthew.closson@gmail.com>
2012-09-30 09:45:54 -0400

    Added supplier database model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120930134221) do
+ActiveRecord::Schema.define(:version => 20120930134534) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -131,6 +131,16 @@ ActiveRecord::Schema.define(:version => 20120930134221) do
     t.integer  "season_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+  end
+
+  create_table "suppliers", :force => true do |t|
+    t.string   "name"
+    t.string   "telephone"
+    t.text     "notes"
+    t.boolean  "inactive"
+    t.integer  "margin"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
