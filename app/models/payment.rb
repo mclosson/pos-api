6dbcdc6c0d041fd73e5b74418b6fd46de8d2139e
Matthew Closson <matthew.closson@gmail.com>
2012-10-02 02:07:58 -0400

    Setup relationship between payment and type, added this to index view

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,8 @@
 class Payment < ActiveRecord::Base
   attr_accessible :amount, :ticket_id, :payment_type_id
 
+  belongs_to :payment_type
+
   validates :ticket_id, presence: true, numericality: true
   validates :amount, presence: true, numericality: true
   validates :payment_type_id, presence: true, numericality: true
--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -2,6 +2,7 @@ class PaymentType < ActiveRecord::Base
   attr_accessible :description, :location_id
   
   belongs_to :location
+  has_many :payments
 
   def as_json(options = {})
     super(only: [:id, :description])
